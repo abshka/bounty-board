72aa21bd21f834f3560c9097b5c31bfa570e61be
AB <markelofaleksei@gmail.com>
2024-05-16 21:40:28 +0300

    Плитка Строки

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,34 +1,50 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 0
 
-fun main() {
-    println("The hero announces her presence to the world.")
-    println(HERO_NAME)
-
-    var playerLevel = 4
-    println(playerLevel)
-
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
-    val hasBefriendedBarbarians = true
-
-    val quest: String = when {
-        playerLevel == 1 -> "Meet Mr. Bubbles in the land of soft things."
-        playerLevel in 2..5 -> {
+fun obtainQuest(
+    playerLevel: Int,
+    hasAngeredBarbarians: Boolean = false,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true): String {
+    return when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
             val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
             if (canTalkToBarbarians) "Convince the barbarians to call off their invasion."
             else "Save the town from the barbarian invasions."
         }
-        playerLevel == 6 -> "Locate the enchanted sword."
-        playerLevel == 7 -> "Recover the long-lost artifact of creation."
-        playerLevel == 8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
+}
 
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
+fun readBountyBoard() {
+    println(
+        """
+            |$HERO_NAME approaches the bounty board. It reads:
+            |   "${obtainQuest(playerLevel).replace("Nogartse","xxxxxxxx")}"
+        """.trimMargin()
+    )
+}
+
+fun main() {
+    println("$HERO_NAME announces her presence to the world.")
+    println("What level is $HERO_NAME")
+    val playerLevelInput = readLine()!!
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
+        playerLevelInput.toInt()
+    }
+    else{
+        1
+    }
+    println("$HERO_NAME's level is $playerLevel")
+
+    readBountyBoard()
 
     println("Time passes...")
-    println("The hero returns from her quest.")
+    println("$HERO_NAME returns from her quest.")
 
     playerLevel += 1
     println(playerLevel)
