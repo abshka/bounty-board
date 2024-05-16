201971fce4b2f3dd972e9619e7870c6c5261c7da
AB <markelofaleksei@gmail.com>
2024-05-16 19:16:05 +0300

    MagicMirror задание

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -3,6 +3,9 @@ fun main() {
 
     val HERO_NAME = "Madrigal"
     println(HERO_NAME)
+    val reversedName = HERO_NAME.reversed() // lagirdaM
+    println("The mirror shows: $reversedName")
+
     var playerLevel = 4
     println(playerLevel)
 
@@ -10,20 +13,6 @@ fun main() {
     if (!hasSteed) {
         println("$HERO_NAME doesn't have a steed yet.")
     }
-
-    // Переменные для сцены в таверне
-    val tavernName = "The Unicorn's Horn"
-    val innkeeperName = "Brom"
-    var playerCoins = 50
-
-    // Меню напитков в виде списка
-    val drinkMenu = listOf("Mead", "Wine", "Beer")
-
-    println("$HERO_NAME arrives at $tavernName.")
-    println("$innkeeperName the innkeeper asks, \"Do you need a stable?\"")
-    println("\"No,\" $HERO_NAME replies, \"I don't have a steed. But I have $playerCoins coins, and I'd like a drink.\"")
-    println("\"Wonderful!\" says $innkeeperName. \"We have ${drinkMenu.joinToString(", ")} on the menu. What would you like?\"")
-
 
 
     println("The hero embarks on her journey to locate the enchanted sword.")
