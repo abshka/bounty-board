9d078ee975514d08a419f42475c67df027a89bf6
AB <markelofaleksei@gmail.com>
2024-05-16 18:48:54 +0300

    Initialization

---
+++ src/main/java/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("The hero announces her presence to the world")
+
+    val HERO_NAME = "Madrigal"
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+}
