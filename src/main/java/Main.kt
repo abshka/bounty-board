8f99100e3bf18e809f9a40d538d87a96da124e5b
AB <markelofaleksei@gmail.com>
2024-05-16 20:10:29 +0300

    Плитка Условия

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,14 +1,35 @@
-fun main(){
-    println("The hero announces her presence to the world")
+const val HERO_NAME = "Madrigal"
 
-    val HERO_NAME = "Madrigal"
+fun main() {
+    println("The hero announces her presence to the world.")
     println(HERO_NAME)
+
     var playerLevel = 4
     println(playerLevel)
 
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val hasBefriendedBarbarians = true
 
+    val quest: String = when {
+        playerLevel == 1 -> "Meet Mr. Bubbles in the land of soft things."
+        playerLevel in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) "Convince the barbarians to call off their invasion."
+            else "Save the town from the barbarian invasions."
+        }
+        playerLevel == 6 -> "Locate the enchanted sword."
+        playerLevel == 7 -> "Recover the long-lost artifact of creation."
+        playerLevel == 8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
+    }
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel += 1
     println(playerLevel)
 }
